01fcb6c65068a1d71858e152cd15ab120ba596cc
Benjamin2955 <ozenbunyamin29@gmail.com>
2021-12-27 01:42:20 -0600

    added new function

--- src/test/java/com/cydeo/tests/test01.java
+++ src/test/java/com/cydeo/tests/test01.java
@@ -3,5 +3,6 @@ package com.cydeo.tests;
 public class test01 {
     public static void main(String[] args) {
         System.out.println("Test1 created");
+        System.out.println("Added new function");
     }
 }
